80a4648b4551ff30440807e940e3f16a9cff10fc
ippei <7588121@gmaim.com>
2021-07-30 08:26:51 +0900

    モデル作成時にファクトリを自動的に作成させるよう変更

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,6 @@ module TaskManagementTool
     # config.eager_load_paths << Rails.root.join("extras")
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
       view_specs: false,
       helper_specs: false,  
       routing_specs: false

036d233aa671127cc29567ea1458351da16cb14e
ippei <7588121@gmaim.com>
2021-08-05 21:29:51 +0900

    [update] プロジェクト名の有無をテスト

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  let(:project) {create(:project)}
+  let(:project) {build(:project,name:'株式会社ABC_RPA案件')}
+  
   describe "attribute: name" do
-    it "ユーザー単位で重複したプロジェクト名を許可しないこと" do
+    #プロジェクト名が存在する場合、有効であること
+    context "when name is present" do
+      it "is valid" do
+        expect(project).to be_valid
+      end
+      end
+    #プロジェクト名が存在しない場合、無効であること
+    context "when name is not present" do
+      it "is invalid" do
+        project.name = ''
+        expect(project).to be_invalid
+        expect(project.errors[:name]).to include("を入力してください")
+      end
+    end
+    it  do
       #最初のプロジェクトを作成
       project
       #「最初のプロジェクト」と同じユーザーで、同一プロジェクトのモデルを生成

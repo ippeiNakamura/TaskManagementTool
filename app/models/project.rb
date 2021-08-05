83e74d5da11adb8f7be65ba3a3b81ef88d17f4d2
ippei <7588121@gmaim.com>
2021-08-05 21:36:00 +0900

    [add]空白を許可しないバリデーションの追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
     belongs_to :user,optional: true
     has_many :work_targets,dependent: :destroy
-    validates :name,uniqueness: {scope: :user_id}
+    validates :name,uniqueness: {scope: :user_id},presence: true
     scope :set_projects, -> { where(:attibute => value)}
     # Ex:- scope :active, -> {where(:active => true)}
     

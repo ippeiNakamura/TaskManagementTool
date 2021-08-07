97580ac543e21c8a479376e0cfa17c4862e93644
ippei <7588121@gmaim.com>
2021-08-07 18:53:08 +0900

    add: exampleの追加 「作業対象が存在する/しない場合の有効性」チェック

--- app/models/work_target.rb
+++ app/models/work_target.rb
@@ -1,4 +1,5 @@
 class WorkTarget < ApplicationRecord
     belongs_to :project,optional: true
     has_many :flags,dependent: :destroy
+    validates :name,uniqueness:{scope: :project_id,message: "作業対象は既に存在します"},presence: true
 end
--- spec/models/work_target_spec.rb
+++ spec/models/work_target_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe WorkTarget, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attribute: name" do
+    let(:user) {create(:user)}
+    let(:project) {create(:project,user: user)}
+    let(:build_work_target) {build(:work_target,name:"売り上げ集計表作成ロボ",project: project)}
+    
+    context "作業対象が存在する場合" do
+      fit "有効であること" do
+        expect(build_work_target).to be_valid
+      end
+    end
+    context "作業対象が存在しない場合" do
+      subject { page } 
+      fit "無効であること" do
+        build_work_target.name = nil
+        expect(build_work_target).to be_invalid
+
+        build_work_target.name = ""
+        expect(build_work_target).to be_invalid
+
+        build_work_target.name = " "
+        expect(build_work_target).to be_invalid
+      end
+
+    end
+    
+  end
 end

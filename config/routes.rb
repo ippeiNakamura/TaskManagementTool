c2e37fa3d06848e4cd97187c2051dc9bf0aeb615
ippei <7588121@gmaim.com>
2021-08-09 07:46:58 +0900

    add: 「routes.rb」にshallowメソッドを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,26 @@
 Rails.application.routes.draw do
   root 'users#index'
-  resources :users do
-    resources :projects do
-      resources :work_targets do
-        resources :flags do
-          resource :top,only: [:show]
-          resources :tasks do
+  shallow do
+    resources :users do
+      resources :projects do
+        resources :work_targets do
+          resources :flags do
+            resources :tasks do 
+            end
           end
+        end
+      end
+    end
+  end      
+          resource :top,only: [:show]
+         
           resources :schedules do
               collection do
                 get 'get_category_children', defaults: { format: 'json' }
                 get 'get_category_grandchildren', defaults: { format: 'json' }
               end
-          #get 'ajax',to: 'schedules#ajax'
-          end
-        end
+          #get 'ajax',to: 'schedules#ajax
       end
-    end
-  end
 
   resource :session,only: [:create,:destroy]
 end

5cc550970ef65f4e2b5f84ff85c73bba6ed1cb9e
ippei <7588121@gmaim.com>
2021-08-28 09:22:16 +0900

    add タスク一覧取得のAPI作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,10 +3,7 @@
     def index
       user = User.find(params[:user_id])
       #user_tasks = user.projects.includes(work_targets: [flags: :tasks])
-      user_tasks = User.includes(projects: [work_targets: [flags: :tasks]]).find(params[:user_id])
-      binding.pry
-      
-      
+      render json: Task.all
     end
 
     def new
